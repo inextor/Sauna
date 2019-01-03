aee8d265f33cd83fcff04b959a07b6a43cb1da1c
Nextor Leon <nextor@gmail.com>
2019-01-02 16:57:53 -0800

    on dev

--- js/Page.js
+++ js/Page.js
@@ -1,119 +1,206 @@
-class Page
+const template = document.createElement('template');
+
+template.innerHTML = `
+  <style>
+
+    :host {
+  		position				: fixed;
+  		top						: 0;
+  		left					: 0;
+  		right					: 0;
+  		bottom					: 0;
+  		/*width					: 100%;*/
+  		overflow				: hidden;
+  		z-index					: 1;
+  		box-sizing			: border-box;
+  		transition				: all 0.5s ease;
+    }
+
+	:host([animation="enabled"][disabled])
+	{
+  		-moz-transition-property: none;
+  		-webkit-transition-property: none;
+  		-o-transition-property: none;
+  		transition-property: none;
+		transition				: unset;
+	}
+
+	:host([status="previous"])
+	{
+	  position				: fixed;
+	  z-index					: 1;
+	  -webkit-transform		: translate3d(-100%,0,0);
+	  transform				: translate3d(-100%,0,0);
+	}
+
+	:host([status="active"])
+	{
+	  z-index					: 3;
+	  display					: block;
+	  -webkit-transform		: translate3d(0,0,0);
+	  transform				: translate3d(0,0,0);
+	}
+
+	.wrapper
+	{
+		display: -ms-flexbox;
+    	display: -webkit-flex;
+    	display: flex;
+    	-webkit-flex-direction: column;
+    	-ms-flex-direction: column;
+    	flex-direction: column;
+    	-webkit-flex-wrap: nowrap;
+    	-ms-flex-wrap: nowrap;
+    	flex-wrap: nowrap;
+    	-webkit-justify-content: flex-start;
+    	-ms-flex-pack: start;
+    	justify-content: flex-start;
+    	-webkit-align-content: stretch;
+    	-ms-flex-line-pack: stretch;
+    	align-content: stretch;
+    	-webkit-align-items: flex-start;
+    	-ms-flex-align: start;
+    	align-items: flex-start;
+
+		width: 100%;
+		height: 100%
+	}
+
+	.wrapper>.main
+	{
+
+		-webkit-order: 0;
+    	-ms-flex-order: 0;
+    	order: 0;
+    	-webkit-flex: 1 0 auto;
+    	-ms-flex: 1 0 auto;
+    	flex: 1 0 auto;
+
+   		-webkit-align-self: stretch;
+    	-ms-flex-item-align: stretch;
+    	align-self: stretch;
+
+
+		overflow-y				: auto;
+		overflow-x				: hidden;
+	}
+/*
+	:host.noanimation
+	{
+	  z-index					: 2;
+	  transition				: all 0s linear;
+	  -moz-transition-property: none;
+	  -webkit-transition-property: none;
+	  -o-transition-property: none;
+	  transition-property: none;
+	}
+
+	:host.previous.noanimation
+	{
+
+	  z-index					: 1;
+	  transition				: all 0s linear;
+	  transform				: translate3d(100%,0,0);
+	  -moz-transition-property: none;
+	  -webkit-transition-property: none;
+	  -o-transition-property: none;
+	  transition-property: none;
+
+	}
+
+	:host
+	,:host.start.previous
+	{
+	  transform				: translate3d(-100%,0,0);
+	  -webkit-transform		: translate3d(-100%,0,0);
+	  display					: block;
+	}
+
+	:host header
+	{
+	  height					: 45px;
+	  position				: absolute;
+	  top						: 0;
+	  right					: 0;
+	  left					: 0;
+	}
+*/
+
+  </style>
+  <div class="wrapper">
+	<div class="main">
+		<slot></slot>
+	</div>
+  </div>
+`;
+
+class Page extends HTMLElement
 {
 	/*
 	*/
-	constructor( options )
+	constructor()
 	{
-		this._selector	= null;
+		super();
 
-		this.options =
-		{
-			element			: null
-			,removeOnPop	: false
-			,template		: null
-		};
+		this.debug = true;
 
-		if( options )
-			this._initOptions( options );
-
-		this._isInitialized	= false;
+		let shadowRoot = this.attachShadow({mode: 'open'});
+    	shadowRoot.appendChild(template.content.cloneNode(true));
 	}
 
-	_initOptions(options)
+	connectedCallback()
 	{
+		console.log('connected');
+	}
 
-		let keys = Object.keys( options );
+	disconnectedCallback()
+	{
+		console.log('disconnected');
+	}
 
-		for(let key in options )
+	attributeChangedCallback(attrName, oldVal, newVal)
+	{
+		if( this.debug )
 		{
-			if( key in this.options )
-				this.options[ key ] = options[ key ];
+			console.log( 'Changing var '+attrName,oldVal,newVal );
 		}
+  	}
 
-		if("element" in options )
+	pushIn()
+	{
+		console.log('pushIn'+this.getAttribute('id'));
+		if( this.getAttribute("status") == "previous" && this.getAttribute("animation") == "enabled" )
 		{
-			if( typeof options.element === "string" )
-			{
-				this._selector	= options.element;
-				this._element	= document.querySelector( this._selector );
-
-				if( this._element  === null )
-				{
-					throw 'Initializator for new Nage('+domOrSelector+')  is a bad selector';
-				}
-			}
-			else if( options.element instanceof HTMLElement )
-			{
-				this._element = options.element;
-			}
+			//this.removeAttribute("animation");
+			this.setAttribute("disabled","");
+			this.removeAttribute("status");
+			this.removeAttribute("disabled");
 		}
-		else if( "template" in options )
+		this.setAttribute("status","active");
+	}
+
+	pushOut()
+	{
+		console.log('pushOut '+this.getAttribute('id'));
+		if( this.getAttribute("status") !== "previous" && this.getAttribute("animation") == "enabled" )
 		{
-			let element = window.document.createElement('div');
-			element.classList.add( 'page' );
-			element.innerHTML = options.template;
-			document.body.appendChild( element );
-			this._element = element;
+			console.log('No Status');
+			this.setAttribute("disabled","");
+			this.setAttribute("status","previous");
+			this.removeAttribute("disabled");
 		}
+		this.setAttribute("status","active");
 	}
-
-	getId()
+	popIn()
 	{
-		if( this._element )
-			return this._element.getAttribute('id');
-
-		return null;
+		console.log('popIn '+this.getAttribute('id'));
+		this.setAttribute("status","previous");
 	}
-
-	setId( id )
+	popOut()
 	{
-		if( this._element.getAttribute('id') === null )
-		{
-			this._element.setAttribute('id', id );
-		}
-	}
-
-	onHide()
-	{
-		console.log('onHide', this.getId() );
-	}
-
-	setOnInit( func )
-	{
-		this._onInit = func;
-	}
-
-	onInit()
-	{
-		if( this._onInit )
-		{
-			this._onInit();
-		}
-		else
-		{
-			console.log('onInit', this.getId() );
-		}
-	}
-	onRemove()
-	{
-		console.log('onRemove', this.getId() );
-	}
-
-	onload()
-	{
-		console.log('PageLoad', this.getId() );
-	}
-
-
-	setOnShow(func)
-	{
-		this._onShow = func;
-	}
-	onShow()
-	{
-		if( this._onShow  )
-			this._onShow();
-		else
-			console.log('PageShow', this.getId() );
+		console.log('popOut '+this.getAttribute('id'));
+		this.removeAttribute("status");
 	}
 }
+
+customElements.define('sauna-page', Page);
